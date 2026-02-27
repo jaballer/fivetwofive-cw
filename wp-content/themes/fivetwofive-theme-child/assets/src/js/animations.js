c9343a276830f42518b036042aacb8e2c42f9c7a
Jabal Torres <info@jabaltorres.com>
2026-02-26 18:21:52 -0800

    temp removed animations

--- wp-content/themes/fivetwofive-theme-child/assets/src/js/animations.js
+++ wp-content/themes/fivetwofive-theme-child/assets/src/js/animations.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function() {
     //     initInnerPageAnimations();
     // }
 
-    initGlobalAnimations();
+    // initGlobalAnimations();
 });
 
 /**
