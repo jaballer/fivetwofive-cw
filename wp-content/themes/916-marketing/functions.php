329edd57d6d1b9633e1a53ad87d20f12cf79de8a
Jabal Torres <141190266+jaballer@users.noreply.github.com>
2026-09-14 08:22:55 -0700

    Update functions.php (#184)

--- wp-content/themes/916-marketing/functions.php
+++ wp-content/themes/916-marketing/functions.php
@@ -14,8 +14,8 @@ defined( 'ABSPATH' ) || exit;
 define( 'MARKETING916_VERSION', wp_get_theme()->get( 'Version' ) );
 
 /* Contact details used in the header, contact section, and footer. */
-define( 'MARKETING916_PHONE', '(916) 555-9160' );
-define( 'MARKETING916_PHONE_TEL', '+19165559160' );
+define( 'MARKETING916_PHONE', '(916) 432-7707' );
+define( 'MARKETING916_PHONE_TEL', '+19164327707' );
 define( 'MARKETING916_EMAIL', 'hello@916marketing.com' );
 
 /**
